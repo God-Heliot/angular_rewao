efe0f66ca1e8367482888def277e1dbac1687a0f
Heliot Rojas <fernando.herrera85@gmail.com>
2020-11-12 22:29:08 -0500

    base

--- black-dashboard-angular-master/src/app/components/components.module.ts
+++ black-dashboard-angular-master/src/app/components/components.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FooterComponent } from "./footer/footer.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+// import { NavbarComponent } from "./navbar/navbar.component";
+// NavbarComponent, NavbarComponent
 import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
   imports: [CommonModule, RouterModule, NgbModule],
-  declarations: [FooterComponent, NavbarComponent, SidebarComponent],
-  exports: [FooterComponent, NavbarComponent, SidebarComponent]
+  declarations: [FooterComponent, SidebarComponent],
+  exports: [FooterComponent, SidebarComponent]
 })
 export class ComponentsModule {}
